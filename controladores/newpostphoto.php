68bf2b480437b7ce66d0d239e8b88567cfe73c6a
kuuukooo <jwh743@gmail.com>
2023-08-08 09:14:36 -0400

    Versión Estable de la Galería

--- controladores/newpostphoto.php
+++ controladores/newpostphoto.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 require "../basededatos/basededatos.php";
 
 $image = "";
@@ -7,65 +8,64 @@ $description = $_POST['description'];
 date_default_timezone_set('Etc/GMT-4');
 $date = date('Y-m-d H:i:s');
 
-//para contar las imagenes que se publican
+//Contamos la cantidad de imágenes que queremos publicar:
+$countfiles = count($_FILES['files']['name']);
 
-$countfiles = count($_FILES['files'] ['name']);
-
-//arreglo para guardar los nombres de las imaganes en la base de datos
-
+//Array para guardar los nombres de las imágenes en la bdd:
 $images = array();
 
-// para contar la cantidad de archivos que se van a mostrar
-if($countfiles > 0) {
-//fileTmpPath es el nombre temporal del archivo que se cargará 
-    for($i = 0; $i < $countfiles; $i++) {
+if($countfiles > 0){
+
+    for($i = 0; $i < $countfiles; $i++){
         $fileTmpPath = $_FILES['files']['tmp_name'][$i];
-        $fileName  = $_FILES['files']['name'][$i];
+        $fileName = $_FILES['files']['name'][$i];
         $fileType = $_FILES['files']['type'][$i];
         $fileNameCmps = explode(".", $fileName);
         $fileExtension = strtolower(end($fileNameCmps));
-        $newFileName = md5(time() . $fileName) . "." . $fileExtension;
+        $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
         $image = $newFileName;
 
-        $allowedFileExtensions = array('png','jpg','jpeg');
+        $allowedFileExtensions = array('png', 'jpg', 'jpeg');
+
         if(in_array($fileExtension, $allowedFileExtensions)){
-            $uploadFileDir = '../assets/images/posts';
-            $dest_path  = $uploadFileDir . $newFileName;
-            //comprimir la imagen subida
+            //directorio donde guardamos la imagen
+            $uploadFileDir = './assets/images/posts/';
+            $dest_path = $uploadFileDir . $newFileName;
+
+            //comprimimos la imagen
             $calidad = 40;
             $originalImage = "";
-            if($fileExtension == "png"){
+            if($fileExtension = 'png'){
                 $originalImage = imagecreatefrompng($fileTmpPath);
-            } else {
+            }else{
                 $originalImage = imagecreatefromjpeg($fileTmpPath);
-
             }
 
             if(imagejpeg($originalImage, $dest_path, $calidad)){
                 array_push($images, $image);
-
             }
         }
-    } 
+    }
 
-    $imagelist = implode(",", $images);
+    $imagesList = implode(",", $images);
 
-    $sql = 'INSERT INTO posts (image, description, date) VALUES (:images, :description, :date)';
-    $stmt = $conn -> prepare($sql);
-    
-    $stmt ->bindParam(':images', $imagelist);
-    $stmt ->bindParam(':description', $description);
-    $stmt ->bindParam(':date', $date);
+    $sql = 'INSERT INTO posts (images, description, date) VALUES (:images, :description, :date)';
+    $stmt = $conn->prepare($sql);
 
+
+    $stmt->bindParam(':images', $imagesList);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':date', $date);
 
     if($stmt->execute()){
         $respuesta = "Post publicado correctamente";
         $_SESSION['success'] = $respuesta;
         header('location: ../index.php');
-    }else {
-        $respuesta = "No se pudo publicar el post";
+    }else{
+        $respuesta = "No ha sido posible publicar el post";
         $_SESSION['error'] = $respuesta;
-        header('location: ../index.php');  
+        header('location: ../index.php');
     }
 }
+
 ?>
--- controladores/posts.php
+++ controladores/posts.php
@@ -1,8 +1,10 @@
 <?php
-require("./basededatos/basededatos.php");
+
+require "./basededatos/basededatos.php";
 $posts = array();
-$stmt = $conn->prepare("SELECT * From posts");
+$stmt = $conn->prepare("select * from posts");
 $stmt->execute();
+
 while($row = $results = $stmt->fetch(PDO::FETCH_ASSOC)){
     array_push($posts, $row);
 }
