9f02ea3a46b620d629e0eac24d454d04bf3921c9
José Ramírez <138532719+kuuukooo@users.noreply.github.com>
2023-08-21 08:22:51 -0400

    Código Actualizado - Modal Dark y Day Mode

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- Inicio de la sesión de PHP -->
 <?php
     session_start();
     require "./controllers/posts.php";
@@ -9,10 +10,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba</title>
-    <link rel="stylesheet" type="text/css" href="estilos.css"> </link>
+    <title>Galeria de fotos A.J</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="estilos.css"> </link>
     <script>
     var modals = new bootstrap.Modal(document.body, {
         selector: '.modal'
@@ -20,7 +21,7 @@
     </script>
 </head>
 <body>
-    <!-- Header -->
+<!-- Header -->
 <div class="header"> 
         <div class="logo">
             <img src="./assets/vierci-removebg-preview.png">
@@ -29,7 +30,19 @@
             <h2>Galeria de Fotos</h2>    
         </div>  
 </div>
-        <!-- formulario -->
+<!-- Botón de Cambiado de modo Dia y Noche-->
+<div class="position-absolute top-0 end-0">
+        <div class="form-check form-switch mt-3 me-3">
+        <label class="form-check-label ms-3" for="lightSwitch">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
+            <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+          </svg>
+        </label>
+        <input class="form-check-input" type="checkbox" id="lightSwitch"/>
+      </div>
+      <script src="DiaNoche.js"></script>
+</div> 
+<!-- formulario -->
     <div class="container justify-content-center">
         <div class="mt-5 mx-auto">
             <div>
@@ -59,13 +72,12 @@
         </div>
     </div>
 
-    <!-- Columna de fotos -->
+<!-- Columna de fotos -->
     <div class="container mt-5 mb-5 py 2">
 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-5" >
     <?php for($i = 0; $i < count($posts); $i++){
         $imagesName = explode(",", $posts[$i]['images']);
     ?>
-
         <div class="col" style="width: 275px">
             <div class="carousel slide" id="carousel<?=$posts[$i]['id'] ?>" data-bs-ride="false">
                 <div class="carousel-inner">
@@ -93,7 +105,7 @@
     $imagesName = explode(",", $post['images']);
     foreach ($imagesName as $j => $imageName) {
 ?>
-    <!-- Código del Modal -->
+<!-- Código del Modal -->
     <div class="modal fade" id="modal<?=$post['id']?>-<?=$j?>" tabindex="-1" aria-labelledby="modalLabel<?=$j?>" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
@@ -111,11 +123,11 @@
                         <?php if(count($imagesName) > 1){ ?>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselModal<?=$post['id']?>-<?=$j?>" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
+                            <span class="visually-hidden">Anterior</span>
                         </button>
                         <button class="carousel-control-next" type="button" data-bs-target="#carouselModal<?=$post['id']?>-<?=$j?>" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
+                            <span class="visually-hidden">Siguiente</span>
                         </button>
                         <!-- Indicadores dentro del Modal -->
                         <ol class="carousel-indicators">
