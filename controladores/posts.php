c4d515c0e9f3160db417c36439abadf819e57d89
José Ramírez <138532719+kuuukooo@users.noreply.github.com>
2023-08-29 13:57:09 -0400

    Galeria 4.0 😎

--- controladores/posts.php
+++ controladores/posts.php
@@ -2,7 +2,7 @@
 
 require "./database/database.php";
 $posts = array();
-$stmt = $conn->prepare("select * from posts");
+$stmt = $conn->prepare("select * from imagenes_sueltas");
 $stmt->execute();
 
 while($row = $results = $stmt->fetch(PDO::FETCH_ASSOC)){
